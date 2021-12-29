5491e885c3dc30b6ffa573028bdedc88d4983787
Renato Fagalde <renato@likwi.com.br>
2021-12-28 23:11:12 -0300

    add annotation basePackages = "br.com.likwi.clients"

--- customer/src/main/java/br/com/likwi/customer/CustomerApplication.java
+++ customer/src/main/java/br/com/likwi/customer/CustomerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
+@EnableFeignClients(
+        basePackages = "br.com.likwi.clients"
+)
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
