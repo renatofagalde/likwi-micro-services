cbc7c05293e5102eff9ae7ee920803c8d4a9f4b6
Renato Fagalde <renato@likwi.com.br>
2022-01-01 15:11:57 -0300

    Topic Exchange config with: NotificationConfig

--- notification/src/main/java/br/com/likwi/notification/config/NotificationConfig.java
+++ notification/src/main/java/br/com/likwi/notification/config/NotificationConfig.java
@@ -1,7 +1,12 @@
 package br.com.likwi.notification.config;
 
 import lombok.Data;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -18,4 +23,22 @@ public class NotificationConfig {
 
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
+
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange(this.interalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(this.notificationQueue)
+    }
+
+    @Bean
+    public Binding internalToNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+    }
 }
