f2f590fef7a24cb3956ecf992f3935a3ca4c4a03
Renato Fagalde <renato@likwi.com.br>
2022-01-01 15:00:16 -0300

    Notification application.yml config

--- advanced message queue protocol/src/main/java/br/com/likwi/advancedMessageQueueProtocol/RabbitMQConfig.java
+++ advanced message queue protocol/src/main/java/br/com/likwi/advancedMessageQueueProtocol/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package br.com.likwi.advancedMessageQueueProtocol;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -16,6 +17,7 @@ public class RabbitMQConfig {
 
     private final ConnectionFactory connectionFactory;
 
+    //send a message
     @Bean
     public AmqpTemplate amqpTemplate() {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(this.connectionFactory);
@@ -23,6 +25,14 @@ public class RabbitMQConfig {
         return rabbitTemplate;
     }
 
+    //listener a message
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(this.connectionFactory);
+        factory.setMessageConverter(jackson2JsonMessageConverter());
+        return factory;
+    }
 
     @Bean
     public MessageConverter jackson2JsonMessageConverter() {
